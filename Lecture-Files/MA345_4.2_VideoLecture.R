b916dda69d804692e214b95f7cf716ee00806a51
novak727 <devinkennedy727@gmail.com>
2026-01-20 16:05:42 -0600

    Add files via upload

---
+++ Lecture-Files/MA345_4.2_VideoLecture.R
@@ -0,0 +1,13 @@
+## 4.2
+
+# Running time
+run <- c(6,10,12,11,9,7,9)
+sd(run) # sample standard deviation
+
+## Vitamin C advertising
+(10/500)*100
+
+(7/250)*100
+
+Hawkes.SD <- c(0,-2,0,-2,0,-2,-1)
+sd(Hawkes.SD)
